c6dab0723b3a06945597383563c57e31effa694f
TekniqueJoseph <joejacksun@gmail.com>
2023-04-22 18:48:20 -0400

    added assigmnet

--- assignment.js
+++ assignment.js
@@ -1,1 +1,37 @@
 const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
+let done = false
+while(!done){
+    const randomNumber = Math.random();
+    if (randomNumber > .7) {
+        done = true
+        alert (`its => ${randomNumber}`)
+    }
+}
+
+const digits = [1,2,3,4,5]
+
+for (let i = 0; i < digits.length; i++){
+    console.log(digits[i])
+}
+
+for(const nums of digits){
+    console.log(nums)
+}
+
+for(let i = 4; i >= 0; i--) {
+    console.log(digits[i])
+}
+
+let finished = false;
+while (!finished) {
+    const rndNum = Math.random();
+    const rndNum2 = Math.random()
+    // ran.push(rndNum);
+    // ran2.push(rndNum2)
+
+    if ((rndNum >.7 && rndNum2 > .7) || (rndNum <= .02 || rndNum2 <= .2)) {
+        finished = true
+    }
+    console.log(`The 1st # is ${rndNum} and the 2nd # is ${rndNum2}.`)
+    // console.log(rndNum2)
+}
