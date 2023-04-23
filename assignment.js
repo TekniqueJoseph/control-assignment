f1adf501c723e02e847741a0d390e4dbeae7968c
TekniqueJoseph <joejacksun@gmail.com>
2023-04-23 07:55:03 -0400

    added traditional if else for 1st evaluation

--- assignment.js
+++ assignment.js
@@ -7,6 +7,15 @@
 //         alert (`its => ${randomNumber}`)
 //     }
 // }
+
+    const randomNumber = Math.random();
+    if (randomNumber > .7) { // could do this with the if statement only
+        // done = true
+        console.log(`Success! ${randomNumber} meets the condition.`)
+    } else {
+        console.log(`${randomNumber} does not meet the condition.`)
+    }
+
 // const digits = [1,2,3,4,5]
 
 // let counter = 0;
@@ -27,16 +36,15 @@
 //     console.log(digits[i])
 // }
 
-let finished1 = false;
-    const rndNum = Math.random();
-    const rndNum2 = Math.random()
-    if ((rndNum >.7 && rndNum2 > .7) || (rndNum <= .2 || rndNum2 <= .2)) { 
-        console.log(`Complete. The 1st # is ${rndNum} & the 2nd # is ${rndNum2}.`)
-    } else {
-        console.log('None of the conditions were met.')
-    }
+// let finished1 = false;
+//     const rndNum = Math.random();
+//     const rndNum2 = Math.random()
+//     if ((rndNum >.7 && rndNum2 > .7) || (rndNum <= .2 || rndNum2 <= .2)) { 
+//         console.log(`Complete. The 1st # is ${rndNum} & the 2nd # is ${rndNum2}.`)
+//     } else {
+//         console.log('None of the conditions were met.')
+//     }
   
-
 // let finished2 = false;
 // while (!finished2) { 
 //     const rndNum = Math.random();
